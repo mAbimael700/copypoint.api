bcecd80aa204f036c7dbf7910f2a29d573b6d46f
mAbimael <mabimael700@gmail.com>
2025-07-08 21:22:58 -0600

    PaymentRepository methods added

--- src/main/java/com/copypoint/api/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/copypoint/api/domain/payment/repository/PaymentRepository.java
@@ -3,6 +3,9 @@ package com.copypoint.api.domain.payment.repository;
 import com.copypoint.api.domain.payment.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
+    Optional<Payment> findByGatewayId(String paymentId);
 }
