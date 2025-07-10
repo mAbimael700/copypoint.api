8cc0f283691d0fb7041dff39f689b27dae0c4668
mAbimael <mabimael700@gmail.com>
2025-07-10 17:21:37 -0600

    PaymentAttempt gategayResponse JSON edited

--- src/main/java/com/copypoint/api/domain/paymentattempt/PaymentAttempt.java
+++ src/main/java/com/copypoint/api/domain/paymentattempt/PaymentAttempt.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 
@@ -30,6 +32,7 @@ public class PaymentAttempt {
     private String errorCode;
 
     @Column(name = "gateway_response", columnDefinition = "json")
+    @JdbcTypeCode(SqlTypes.JSON)
     private String gatewayResponse;
 
     @Column(name = "created_at")
