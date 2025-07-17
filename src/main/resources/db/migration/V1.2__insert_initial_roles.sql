e5cd95099c21862f9ea8bb833fbe4f1fd5c6105b
mAbimael <mabimael700@gmail.com>
2025-07-17 16:57:23 -0600

    feat(db): add V0 schema initialization and initial seed data as Flyway migrations

---
+++ src/main/resources/db/migration/V1.2__insert_initial_roles.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.roles (id, name)
+    VALUES
+        (1, 'COPYPOINT_CASHIER'),
+        (2, 'COPYPOINT_SUPERVISOR'),
+        (3, 'STORE_MANAGER'),
+        (4, 'STORE_OWNER'),
+        (5, 'COPYPOINT_MANAGER');
---
+++ src/main/resources/db/migration/V1.3__insert_initial_modules.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.modules (id, name, active)
+    VALUES
+        (1, 'COPYPOINT_SALES', true),
+        (2, 'COPYPOINT_INVENTORY', true),
+        (3, 'COPYPOINT_REPORTS', true),
+        (4, 'COPYPOINT_EMPLOYEE_MANAGEMENT', true),
+        (5, 'STORE_INVENTORY', true),
+        (6, 'STORE_REPORTS', true),
+        (7, 'STORE_EMPLOYEE_MANAGEMENT', true),
+        (8, 'COPYPOINT_MANAGEMENT', true),
+        (9, 'STORE_MANAGEMENT', true);
