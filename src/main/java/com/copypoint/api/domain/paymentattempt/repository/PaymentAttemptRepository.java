39b840550ad9a4dd3dc9f6a16bffc8d6d0932703
mAbimael <mabimael700@gmail.com>
2025-07-29 20:07:43 -0600

    PaymentCheckout Controller added - Checkout Gateway Response parser implementation added

--- src/main/java/com/copypoint/api/domain/paymentattempt/repository/PaymentAttemptRepository.java
+++ src/main/java/com/copypoint/api/domain/paymentattempt/repository/PaymentAttemptRepository.java
@@ -5,11 +5,13 @@ import com.copypoint.api.domain.paymentattempt.entity.PaymentAttemptStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PaymentAttemptRepository extends JpaRepository<PaymentAttempt, Long> {
     /* Encuentra todos los intentos de pago para un payment específico, ordenados por fecha de creación descendente
      */
